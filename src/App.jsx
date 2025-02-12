dc61d4877662bb1a35bc19c2f3df9813610e92c8
Manoj-146 <manojmurugesanofficial@gamil.com>
2025-02-13 02:20:23 +0530

    "fix : Header name changed"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import Header from './components/header';
+import Header from './components/Header';
 import MainSection from './components/MainSection';
 import About from './components/About';
 import Project from './components/Project';
