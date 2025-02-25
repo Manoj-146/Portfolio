d4572e089bcfa684d1ef6c1fbdc3f5c9e99fdc3c
Manoj-146 <manojmurugesanofficial@gamil.com>
2025-02-25 16:35:31 +0530

    "Style added"

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,12 +2,12 @@ import Manoj from '../assets/Manoj.jpeg';
 
 const About=()=>{
     return(
-        <div id='About' className="flex flex-row items-center justify-around p-2 bg-[#808080] py-10">
+        <div id='About' className="flex flex-row items-center justify-around p-2 bg-[#808080] py-10 text-white">
             <div className='hidden md:block w-1/4 p-4'>
                 <img src={Manoj} className='rounded-t-full' alt="" />
             </div>
             <div className="w-1/2 flex flex-col">
-                <h1 className="text-4xl font-bold text-center mb-7 pt-5">About</h1>
+                <h1 className="text-4xl font-bold text-center mb-7 pt-5 text-gray-950">About</h1>
                 <ul className='list-disc pb-5'>
                     <li>
                         <p className="text-xl">
@@ -17,14 +17,19 @@ const About=()=>{
                     <br />
                     <li>
                         <p className="text-xl">
-                            In frontened I have knowledge on HTML, CSS, JavaScript, Tailwind.CSS, React.js.
+                            In frontened I have knowledge on <span className='font-bold text-gray-900'>HTML, CSS, JavaScript, React.js, Tailwind.CSS</span>.
                         </p>
                     </li>
                     <br />
                     <li>
                         <p className="text-xl">
-                            In backend I have knowledge on java, Springboot, Mysql, Hibernate, JPA, RestAPI.
-                            I have one year of experience in the BPO sector, which helped me develop customer handling skills and basic work etiquette.
+                            In backend I have knowledge on <span className='font-bold text-gray-900'>Node.js, Express.js, java, Springboot, Hibernate, JPA, RestAPI</span>.
+                        </p>
+                    </li>
+                    <br />
+                    <li>
+                        <p className="text-xl">
+                            In databse I have knowledge on <span className='font-bold text-gray-900'>MySQL, MongoDB</span> and I have used those tehnologies in my projects.
                         </p>
                     </li>
                     <br />
