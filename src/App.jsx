d31074a8968ad30da2a2b67ee871bf638491dc79
Manoj-146 <manojmurugesanofficial@gamil.com>
2025-03-04 23:04:51 +0530

    Lazy loading added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
-import Header from './components/Header';
-import MainSection from './components/MainSection';
-import About from './components/About';
-import Project from './components/Project';
-import Resume from './components/Resume';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
+import React,{ Suspense } from 'react';
+const Header=React.lazy(()=>import ('./components/Header'));
+const MainSection=React.lazy(()=>import ('./components/MainSection'));
+const About=React.lazy(()=>import ('./components/About'));
+const Project=React.lazy(()=>import ('./components/Project'));
+const Resume=React.lazy(()=>import ('./components/Resume'));
+const Contact=React.lazy(()=>import ('./components/Contact'));
+const Footer=React.lazy(()=>import ('./components/Footer'));
 const App=()=>{
 
     return(<>
+        <Suspense fallback={<div>Loading...</div>}>
             <Header/>
             <MainSection/>
             <About/>
@@ -17,6 +17,7 @@ const App=()=>{
             <Resume/>
             <Contact/>
             <Footer/>
+        </Suspense>
     </>);
 }
 
